a5ba40c3cc422e304c4456c297757d1fc6ce9f6b
SDE学徒 <530413575@qq.com>
2015-03-12 12:26:41 +0800

    Revert "dd"
    This reverts commit 55bfde47ad9e969f534abe2b5830093737021ff2.

--- 22/main.cpp
+++ 22/main.cpp
@@ -1,29 +1,2 @@
-#include <stdio.h>
-//#include <iostream>
 #include <algorithm>
-
 using namespace std;
-
-bool cmp(int a, int b)
-{
-    return( a>b );
-}
-
-int main()
-{
-    int n;
-    int buf[100];
-    while(scanf("%d",&n)!=EOF)
-    {
-        for(int i=0; i<n; i++)
-        {
-            scanf("%d",&buf[i]);
-        }
-        sort(buf,buf+n,cmp);
-        for(int i=0; i<n; i++)
-        {
-            //printf("%d ",buf[i]);
-        }
-    }
-    return 0;
-}
